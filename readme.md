763b548fcf0a5b8c93200a61d670bec345978b09
JIANGWEILONGGGG <william@jwlchina.cn>
2017-11-30 14:16:04 +0800

    update

--- readme.md
+++ readme.md
@@ -1,1 +1,23 @@
 # 豆瓣租房信息管理系统
+
+## require
+
+`PHP>=7.0`
+ 
+`mysql`
+
+## 安装
+
+```bash
+cd /path/to/your/application
+composer install
+cp .env.example .env
+php artisan key:generate
+```
+
+配置`.env`文件
+
+
+
+导入`sql/init.sql`文件到MYSQL数据库
+
--- sql/init.sql
+++ sql/init.sql
@@ -16,6 +16,179 @@
 CREATE DATABASE IF NOT EXISTS `douban` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `douban`;
 
+-- 导出  表 douban.admin_menu 结构
+CREATE TABLE IF NOT EXISTS `admin_menu` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL DEFAULT '0',
+  `order` int(11) NOT NULL DEFAULT '0',
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `icon` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uri` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_menu 的数据：~13 rows (大约)
+/*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
+INSERT INTO `admin_menu` (`id`, `parent_id`, `order`, `title`, `icon`, `uri`, `created_at`, `updated_at`) VALUES
+  (1, 2, 9, 'Index', 'fa-bar-chart', '/', NULL, '2017-11-28 17:01:33'),
+  (2, 0, 3, '系统管理', 'fa-tasks', NULL, NULL, '2017-11-28 17:01:33'),
+  (3, 2, 5, '用户管理', 'fa-users', 'auth/users', NULL, '2017-11-28 17:01:33'),
+  (4, 2, 6, '系统角色管理', 'fa-user', 'auth/roles', NULL, '2017-11-28 17:01:33'),
+  (5, 2, 7, '权限管理', 'fa-ban', 'auth/permissions', NULL, '2017-11-28 17:01:33'),
+  (6, 2, 4, '菜单管理', 'fa-bars', 'auth/menu', NULL, '2017-11-28 17:01:33'),
+  (7, 2, 8, '操作日志', 'fa-history', 'auth/logs', NULL, '2017-11-28 17:01:33'),
+  (8, 2, 10, '定时任务', 'fa-clock-o', 'scheduling', '2017-11-28 11:24:14', '2017-11-28 17:01:33'),
+  (9, 0, 11, 'Helpers', 'fa-gears', '', '2017-11-28 11:25:15', '2017-11-28 16:51:29'),
+  (10, 9, 12, 'Scaffold', 'fa-keyboard-o', 'helpers/scaffold', '2017-11-28 11:25:15', '2017-11-28 16:51:29'),
+  (11, 9, 13, 'Database terminal', 'fa-database', 'helpers/terminal/database', '2017-11-28 11:25:15', '2017-11-28 16:51:29'),
+  (12, 9, 14, 'Laravel artisan', 'fa-terminal', 'helpers/terminal/artisan', '2017-11-28 11:25:15', '2017-11-28 16:51:29'),
+  (13, 9, 15, 'Routes', 'fa-list-alt', 'helpers/routes', '2017-11-28 11:25:15', '2017-11-28 16:51:29'),
+  (14, 0, 1, '豆瓣租房信息', 'fa-circle', 'douban', '2017-11-28 11:38:20', '2017-11-28 17:01:33'),
+  (15, 0, 2, '我的收藏', 'fa-star', '/douban_star', '2017-11-28 16:51:24', '2017-11-28 17:01:33');
+/*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_operation_log 结构
+CREATE TABLE IF NOT EXISTS `admin_operation_log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `method` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ip` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `input` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `admin_operation_log_user_id_index` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_operation_log 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `admin_operation_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin_operation_log` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_permissions 结构
+CREATE TABLE IF NOT EXISTS `admin_permissions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `http_method` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `http_path` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `admin_permissions_name_unique` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_permissions 的数据：~7 rows (大约)
+/*!40000 ALTER TABLE `admin_permissions` DISABLE KEYS */;
+INSERT INTO `admin_permissions` (`id`, `name`, `slug`, `http_method`, `http_path`, `created_at`, `updated_at`) VALUES
+  (1, 'All permission', '*', '', '*', NULL, NULL),
+  (2, 'Dashboard', 'dashboard', 'GET', '/', NULL, NULL),
+  (3, 'Login', 'auth.login', '', '/auth/login\r\n/auth/logout', NULL, NULL),
+  (4, 'User setting', 'auth.setting', 'GET,PUT', '/auth/setting', NULL, NULL),
+  (5, 'Auth management', 'auth.management', '', '/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs', NULL, NULL),
+  (6, 'Scheduling', 'ext.scheduling', NULL, '/scheduling*', '2017-11-28 11:24:14', '2017-11-28 11:24:14'),
+  (7, 'Admin helpers', 'ext.helpers', NULL, '/helpers/*', '2017-11-28 11:25:15', '2017-11-28 11:25:15');
+/*!40000 ALTER TABLE `admin_permissions` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_roles 结构
+CREATE TABLE IF NOT EXISTS `admin_roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `admin_roles_name_unique` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_roles 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `admin_roles` DISABLE KEYS */;
+INSERT INTO `admin_roles` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
+  (1, 'Administrator', 'administrator', '2017-11-28 08:22:36', '2017-11-28 08:22:36');
+/*!40000 ALTER TABLE `admin_roles` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_role_menu 结构
+CREATE TABLE IF NOT EXISTS `admin_role_menu` (
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  KEY `admin_role_menu_role_id_menu_id_index` (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_role_menu 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `admin_role_menu` DISABLE KEYS */;
+INSERT INTO `admin_role_menu` (`role_id`, `menu_id`, `created_at`, `updated_at`) VALUES
+  (1, 2, NULL, NULL),
+  (1, 14, NULL, NULL),
+  (1, 15, NULL, NULL);
+/*!40000 ALTER TABLE `admin_role_menu` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_role_permissions 结构
+CREATE TABLE IF NOT EXISTS `admin_role_permissions` (
+  `role_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  KEY `admin_role_permissions_role_id_permission_id_index` (`role_id`,`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_role_permissions 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `admin_role_permissions` DISABLE KEYS */;
+INSERT INTO `admin_role_permissions` (`role_id`, `permission_id`, `created_at`, `updated_at`) VALUES
+  (1, 1, NULL, NULL);
+/*!40000 ALTER TABLE `admin_role_permissions` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_role_users 结构
+CREATE TABLE IF NOT EXISTS `admin_role_users` (
+  `role_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  KEY `admin_role_users_role_id_user_id_index` (`role_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_role_users 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `admin_role_users` DISABLE KEYS */;
+INSERT INTO `admin_role_users` (`role_id`, `user_id`, `created_at`, `updated_at`) VALUES
+  (1, 1, NULL, NULL);
+/*!40000 ALTER TABLE `admin_role_users` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_users 结构
+CREATE TABLE IF NOT EXISTS `admin_users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `admin_users_username_unique` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_users 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
+INSERT INTO `admin_users` (`id`, `username`, `password`, `name`, `avatar`, `remember_token`, `created_at`, `updated_at`) VALUES
+  (1, 'admin', '$2y$10$XfEf9ja0miP.yeW8r7sdCerfhpELbjX9s0Rs4ObypnduYPtiPROzO', '大佬', NULL, NULL, '2017-11-28 08:22:36', '2017-11-28 15:59:00');
+/*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
+
+-- 导出  表 douban.admin_user_permissions 结构
+CREATE TABLE IF NOT EXISTS `admin_user_permissions` (
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  KEY `admin_user_permissions_user_id_permission_id_index` (`user_id`,`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.admin_user_permissions 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `admin_user_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin_user_permissions` ENABLE KEYS */;
+
 -- 导出  表 douban.group 结构
 CREATE TABLE IF NOT EXISTS `group` (
   `url` varchar(200) CHARACTER SET utf8 NOT NULL,
@@ -35,7 +208,10 @@ CREATE TABLE IF NOT EXISTS `group` (
   KEY `reply_num` (`reply_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- 数据导出被取消选择。
+-- 正在导出表  douban.group 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
+
 -- 导出  表 douban.group_mark 结构
 CREATE TABLE IF NOT EXISTS `group_mark` (
   `url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -44,3 +220,56 @@ CREATE TABLE IF NOT EXISTS `group_mark` (
   `value` int(1) NOT NULL,
   PRIMARY KEY (`url`,`type`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.group_mark 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `group_mark` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group_mark` ENABLE KEYS */;
+
+-- 导出  表 douban.migrations 结构
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.migrations 的数据：~2 rows (大约)
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+  (1, '2014_10_12_000000_create_users_table', 1),
+  (2, '2014_10_12_100000_create_password_resets_table', 1),
+  (3, '2016_01_04_173148_create_admin_tables', 1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- 导出  表 douban.password_resets 结构
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.password_resets 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
+-- 导出  表 douban.users 结构
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  douban.users 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
